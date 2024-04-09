6f8783fa0d81acd86f5238ca523722f3c21d88bf
MirArfan <mir.arfan.iiuc9@gmail.com>
2024-04-09 18:07:48 +0600

    learn how to make post request with josn or from data

--- ex-http-request/index.js
+++ ex-http-request/index.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const bodyParser = require('body-parser')
 
 
 ///.......header er moddome res received kora.....///////////
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+app.post("/user", (req, res)=>{
+    const name=req.body.name;
+    res.send(`welcome ${name}`);
+})
 
 app.get("/", (req, res) => {
     const id=req.header('id');
