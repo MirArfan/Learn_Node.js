a6214a16e5985b3f15b10150d529db46eef08ed8
MirArfan <mir.arfan.iiuc9@gmail.com>
2024-04-13 12:54:51 +0600

    commit

--- expressjs-02/index.js
+++ expressjs-02/index.js
@@ -1,6 +1,19 @@
-const app=require('./app')
-const port=3003;
+// const app=require('./app')
+const express=require("express");
+const app=express();
 
+const port=3000;
+
+
+app.get("/products/:id([0-9]+)", (req, res)=>{
+    res.send(` id = ${req.params.id}`);
+})
+
+app.use("*", (req, res)=>{
+    res.status(404).send({
+        message: "not valid route"
+    })
+})
 
 app.listen(port, ()=>{
     console.log("server is runnig at 3000 port");
